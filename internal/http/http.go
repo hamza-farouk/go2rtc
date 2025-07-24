8669dfb098e68e04ee934e4082c2b51301f301f6
Hamza_fk <faroukhamza2@gmail.com>
2025-07-24 21:43:19 +0100

    Update http.go

--- internal/http/http.go
+++ internal/http/http.go
@@ -7,15 +7,15 @@ import (
 	"net/url"
 	"strings"
 
-	"github.com/AlexxIT/go2rtc/internal/api"
-	"github.com/AlexxIT/go2rtc/internal/streams"
-	"github.com/AlexxIT/go2rtc/pkg/core"
-	"github.com/AlexxIT/go2rtc/pkg/hls"
-	"github.com/AlexxIT/go2rtc/pkg/image"
-	"github.com/AlexxIT/go2rtc/pkg/magic"
-	"github.com/AlexxIT/go2rtc/pkg/mpjpeg"
-	"github.com/AlexxIT/go2rtc/pkg/pcm"
-	"github.com/AlexxIT/go2rtc/pkg/tcp"
+	"github.com/hamza-farouk/go2rtc/internal/api"
+	"github.com/hamza-farouk/go2rtc/internal/streams"
+	"github.com/hamza-farouk/go2rtc/pkg/core"
+	"github.com/hamza-farouk/go2rtc/pkg/hls"
+	"github.com/hamza-farouk/go2rtc/pkg/image"
+	"github.com/hamza-farouk/go2rtc/pkg/magic"
+	"github.com/hamza-farouk/go2rtc/pkg/mpjpeg"
+	"github.com/hamza-farouk/go2rtc/pkg/pcm"
+	"github.com/hamza-farouk/go2rtc/pkg/tcp"
 )
 
 func Init() {

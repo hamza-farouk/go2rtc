fd582f10b1f723e5133abf0d59d9eedfce3ae933
Hamza_fk <faroukhamza2@gmail.com>
2025-07-24 22:32:41 +0100

    Update client.go

--- pkg/isapi/client.go
+++ pkg/isapi/client.go
@@ -7,8 +7,8 @@ import (
 	"net/http"
 	"net/url"
 
-	"github.com/AlexxIT/go2rtc/pkg/core"
-	"github.com/AlexxIT/go2rtc/pkg/tcp"
+	"github.com/hamza-farouk/go2rtc/pkg/core"
+	"github.com/hamza-farouk/go2rtc/pkg/tcp"
 )
 
 // Deprecated: should be rewritten to core.Connection

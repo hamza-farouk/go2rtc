91f7de573ccf0998e1ce667aeba65b64c677d1f7
Hamza_fk <faroukhamza2@gmail.com>
2025-07-24 21:43:50 +0100

    Update init.go

--- internal/isapi/init.go
+++ internal/isapi/init.go
@@ -1,9 +1,9 @@
 package isapi
 
 import (
-	"github.com/AlexxIT/go2rtc/internal/streams"
-	"github.com/AlexxIT/go2rtc/pkg/core"
-	"github.com/AlexxIT/go2rtc/pkg/isapi"
+	"github.com/hamza-farouk/go2rtc/internal/streams"
+	"github.com/hamza-farouk/go2rtc/pkg/core"
+	"github.com/hamza-farouk/go2rtc/pkg/isapi"
 )
 
 func Init() {

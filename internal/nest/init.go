465b3e510f8c1bb03509b172c6fe9e18daf4d23c
Hamza_fk <faroukhamza2@gmail.com>
2025-07-24 21:46:51 +0100

    Update init.go

--- internal/nest/init.go
+++ internal/nest/init.go
@@ -4,10 +4,10 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/AlexxIT/go2rtc/internal/api"
-	"github.com/AlexxIT/go2rtc/internal/streams"
-	"github.com/AlexxIT/go2rtc/pkg/core"
-	"github.com/AlexxIT/go2rtc/pkg/nest"
+	"github.com/hamza-farouk/go2rtc/internal/api"
+	"github.com/hamza-farouk/go2rtc/internal/streams"
+	"github.com/hamza-farouk/go2rtc/pkg/core"
+	"github.com/hamza-farouk/go2rtc/pkg/nest"
 )
 
 func Init() {

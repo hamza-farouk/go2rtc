c9b771cd9793b7a14e3ee728b2527f0c7724f3eb
Hamza_fk <faroukhamza2@gmail.com>
2025-07-24 22:00:04 +0100

    Update yandex.go

--- internal/yandex/yandex.go
+++ internal/yandex/yandex.go
@@ -3,9 +3,9 @@ package yandex
 import (
 	"net/url"
 
-	"github.com/AlexxIT/go2rtc/internal/streams"
-	"github.com/AlexxIT/go2rtc/pkg/core"
-	"github.com/AlexxIT/go2rtc/pkg/yandex"
+	"github.com/hamza-farouk/go2rtc/internal/streams"
+	"github.com/hamza-farouk/go2rtc/pkg/core"
+	"github.com/hamza-farouk/go2rtc/pkg/yandex"
 )
 
 func Init() {

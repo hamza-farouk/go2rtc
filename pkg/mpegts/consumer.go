91c4fc9ab7b4e9ef3c25fcb8e75e19046dedff66
Hamza_fk <faroukhamza2@gmail.com>
2025-07-24 22:46:08 +0100

    Update consumer.go

--- pkg/mpegts/consumer.go
+++ pkg/mpegts/consumer.go
@@ -3,10 +3,10 @@ package mpegts
 import (
 	"io"
 
-	"github.com/AlexxIT/go2rtc/pkg/aac"
-	"github.com/AlexxIT/go2rtc/pkg/core"
-	"github.com/AlexxIT/go2rtc/pkg/h264"
-	"github.com/AlexxIT/go2rtc/pkg/h265"
+	"github.com/hamza-farouk/go2rtc/pkg/aac"
+	"github.com/hamza-farouk/go2rtc/pkg/core"
+	"github.com/hamza-farouk/go2rtc/pkg/h264"
+	"github.com/hamza-farouk/go2rtc/pkg/h265"
 	"github.com/pion/rtp"
 )
 

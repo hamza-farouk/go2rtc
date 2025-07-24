7ba7cbacf3518fbfc0988b465741c2305ea8c9e2
Hamza_fk <faroukhamza2@gmail.com>
2025-07-24 22:45:36 +0100

    Update muxer.go

--- pkg/mp4/muxer.go
+++ pkg/mp4/muxer.go
@@ -3,10 +3,10 @@ package mp4
 import (
 	"encoding/hex"
 
-	"github.com/AlexxIT/go2rtc/pkg/core"
-	"github.com/AlexxIT/go2rtc/pkg/h264"
-	"github.com/AlexxIT/go2rtc/pkg/h265"
-	"github.com/AlexxIT/go2rtc/pkg/iso"
+	"github.com/hamza-farouk/go2rtc/pkg/core"
+	"github.com/hamza-farouk/go2rtc/pkg/h264"
+	"github.com/hamza-farouk/go2rtc/pkg/h265"
+	"github.com/hamza-farouk/go2rtc/pkg/iso"
 	"github.com/pion/rtp"
 )
 

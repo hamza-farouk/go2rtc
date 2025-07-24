2b60a71c7e9130007ca795faa0a37074767475a8
Hamza_fk <faroukhamza2@gmail.com>
2025-07-24 22:00:59 +0100

    Update aac.go

--- pkg/aac/aac.go
+++ pkg/aac/aac.go
@@ -4,8 +4,8 @@ import (
 	"encoding/hex"
 	"fmt"
 
-	"github.com/AlexxIT/go2rtc/pkg/bits"
-	"github.com/AlexxIT/go2rtc/pkg/core"
+	"github.com/hamza-farouk/go2rtc/pkg/bits"
+	"github.com/hamza-farouk/go2rtc/pkg/core"
 )
 
 const (

46369ea10a88dd67fee8d3fd9a3a86b6455914cc
Hamza_fk <faroukhamza2@gmail.com>
2025-07-24 22:46:36 +0100

    Update demuxer.go

--- pkg/mpegts/demuxer.go
+++ pkg/mpegts/demuxer.go
@@ -5,9 +5,9 @@ import (
 	"errors"
 	"io"
 
-	"github.com/AlexxIT/go2rtc/pkg/aac"
-	"github.com/AlexxIT/go2rtc/pkg/bits"
-	"github.com/AlexxIT/go2rtc/pkg/h264/annexb"
+	"github.com/hamza-farouk/go2rtc/pkg/aac"
+	"github.com/hamza-farouk/go2rtc/pkg/bits"
+	"github.com/hamza-farouk/go2rtc/pkg/h264/annexb"
 	"github.com/pion/rtp"
 )
 

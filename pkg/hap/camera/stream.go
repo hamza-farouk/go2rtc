3feb6bfc9edd4d37d81216ccb74f8f7996242930
Hamza_fk <faroukhamza2@gmail.com>
2025-07-24 22:21:46 +0100

    Update stream.go

--- pkg/hap/camera/stream.go
+++ pkg/hap/camera/stream.go
@@ -3,9 +3,9 @@ package camera
 import (
 	"errors"
 
-	"github.com/AlexxIT/go2rtc/pkg/core"
-	"github.com/AlexxIT/go2rtc/pkg/hap"
-	"github.com/AlexxIT/go2rtc/pkg/srtp"
+	"github.com/hamza-farouk/go2rtc/pkg/core"
+	"github.com/hamza-farouk/go2rtc/pkg/hap"
+	"github.com/hamza-farouk/go2rtc/pkg/srtp"
 )
 
 type Stream struct {

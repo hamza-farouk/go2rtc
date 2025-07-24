49be01e59094822d25d092ffe2e7cd0c00a1f9de
Hamza_fk <faroukhamza2@gmail.com>
2025-07-24 07:44:52 +0100

    Update echo.go

--- internal/echo/echo.go
+++ internal/echo/echo.go
@@ -4,9 +4,9 @@ import (
 	"bytes"
 	"os/exec"
 
-	"github.com/AlexxIT/go2rtc/internal/app"
-	"github.com/AlexxIT/go2rtc/internal/streams"
-	"github.com/AlexxIT/go2rtc/pkg/shell"
+	"github.com/hamza-farouk/go2rtc/internal/app"
+	"github.com/hamza-farouk/go2rtc/internal/streams"
+	"github.com/hamza-farouk/go2rtc/pkg/shell"
 )
 
 func Init() {

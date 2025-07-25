4a69158b355534ed47067c5b25ed60f9c88b628d
Hamza_fk <faroukhamza2@gmail.com>
2025-07-25 06:15:28 +0100

    Update server.go

--- pkg/rtsp/server.go
+++ pkg/rtsp/server.go
@@ -10,8 +10,8 @@ import (
 	"strconv"
 	"strings"
 
-	"github.com/AlexxIT/go2rtc/pkg/core"
-	"github.com/AlexxIT/go2rtc/pkg/tcp"
+	"github.com/hamza-farouk/go2rtc/pkg/core"
+	"github.com/hamza-farouk/go2rtc/pkg/tcp"
 )
 
 var FailedAuth = errors.New("failed authentication")
